e5b91c8912c40588663726e7f66fbb266bea58b5
Linzee <d.gmiterko@gmail.com>
2015-11-23 10:24:51 +0100

    Better usability

--- 2050.py
+++ 2050.py
@@ -2,7 +2,6 @@
 from PIL import Image
 from Space import Space
 import layers
-import math
 
 WIDTH = 1920
 HEIGHT = 1080
@@ -12,6 +11,44 @@ RENDER_PER_STEP = False
 RENDER_PER_LAYER = False
 BACKGROUND = 'black'
 MAX_LAYERS = None
+SHOW = False
+
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("-W", "--width",
+                  metavar="NUMBER", type="int",
+                  dest="width", default=WIDTH,
+                  help="Width of image")
+parser.add_option("-H", "--height",
+                  metavar="NUMBER", type="int",
+                  dest="height", default=HEIGHT,
+                  help="Height of image")
+parser.add_option("-z", "--zoom",
+                  metavar="NUMBER", type="int",
+                  dest="zoom", default=ZOOM,
+                  help="Zoom represents diameter of largest planet in pixels")
+parser.add_option("-s", "--seed",
+                  metavar="NUMBER", type="int",
+                  dest="seed", default=SEED,
+                  help="Seed used to generate image")
+parser.add_option('-S', "--steps",
+                  action="store_true", dest="steps", default=False,
+                  help="Render each step separately")
+parser.add_option('-L', "--layers",
+                  action="store_true", dest="layers", default=False,
+                  help="Render each layer separately")
+parser.add_option("-t", "--temporary",
+                  action="store_true", dest="temporary", default=False,
+                  help="Store image result in temporary file and display result.")
+options, optionsValues = parser.parse_args()
+
+WIDTH = options.width
+HEIGHT = options.height
+ZOOM = options.zoom
+SEED = options.seed
+RENDER_PER_STEP = options.steps
+RENDER_PER_LAYER = options.layers
+SHOW = options.temporary
 
 import random
 random.seed(SEED)
@@ -20,6 +57,19 @@ usedLayers = [layers.LayerPlanets(), layers.LayerPlanetsSelection(), layers.Laye
 
 im = Image.new("RGB", (WIDTH, HEIGHT), BACKGROUND)
 space = Space(SEED)
+
+def saveImage(image, steps, layers):
+    if SHOW:
+        image.show()
+    else:
+        name = "out"
+        if steps != None:
+            name += "-s" + str(steps)
+        if layers != None:
+            name += "-l" + str(layers)
+        name += ".png"
+        image.save(name, 'PNG')
+        print("Image saved as " + name)
 
 for layer in usedLayers:
     layer.init(space)
@@ -59,14 +109,16 @@ while not allSuccess:
     def transition(coords):
         scalar = type(coords) not in (tuple, list)
         if scalar:
-            coords = (coords, )
+            coords = (coords,)
             scalar = True
         #zoom
         coords = [x * ZOOM for x in coords]
+        
         #offset
         if len(coords) == 2:
             coords[0] += WIDTH / 2
             coords[1] += HEIGHT / 2
+        
         #round
         coords = [int(x) for x in coords]
 
@@ -77,17 +129,21 @@ while not allSuccess:
 
     if RENDER_PER_STEP:
         im.paste(BACKGROUND, (0, 0, WIDTH, HEIGHT))
+        li = 0
         for layer in usedLayers:
             layer.render(space, im, transition)
             if RENDER_PER_LAYER:
-                im.show()
+                saveImage(im, range, li)
+                li += 1
         if not RENDER_PER_LAYER:
-            im.show()
+            saveImage(im, range, None)
 
 if not RENDER_PER_STEP:
+    li = 0
     for layer in usedLayers:
         layer.render(space, im, transition)
         if RENDER_PER_LAYER:
-            im.show()
+            saveImage(im, None, li)
+            li += 1
     if not RENDER_PER_LAYER:
-        im.show()
+        saveImage(im, None, None)
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# py-2050
-Generating images of space based on given parameters.
+![2050 space](out.png)
+
+## py-2050 SK
+Generovanie takýchto obrázkov vesmíru podľa zadaných parametrov. Objekty na obrázku sú generovane postupne pridávaním vrstiev. Jednotlivé vrstvy sú na sebe závislé. Pri tomto programe som si chcel vyskúšať generovanie nekonečného priestoru týmto spôsobom.
+
+Obrázky môžu byť uložené v aktuálnom priečinku alebo len zobrazené.
+
+### Použitie
+```python 2050.py -h```
+
+```python 2050.py -W 800 -H 400 -s 96 -z 80```
+
+## py-2050 EN
+Generating images of space based on given parameters. All objects in image are generated using layers. Layers are applied in given order, and each layer depends on layer before.
+
+Images can be saved in current directory or displayed.
+
+### Usage
+```python 2050.py -h```
+
+```python 2050.py -W 800 -H 400 -s 96 -z 80```
